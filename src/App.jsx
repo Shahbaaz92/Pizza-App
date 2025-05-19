8d07d68021317681c4d8529073a9b1e4de4c0c51
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-05-19 07:14:49 +0530

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // import { useState } from 'react'
+import Pizza from "./components/Pizza";
 
 function App() {
   return (
     <div>
       <h1>Pizza Co.</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
     </div>
   );
 }
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,14 @@
+import { pizzaData } from "../data";
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Name</h2>
+      <p>Ingredients</p>
+      <p>Price</p>
+      <img src="pizzas/focaccia.jpg" alt="Focaccia " />
+    </div>
+  );
+}
+
+export default Pizza;
