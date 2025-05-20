cfcb44ff0853c96487e29c37b2b4a136a8526864
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-05-20 07:14:33 +0530

    Update app

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,10 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.{" "}
+      </p>
       <ul className="pizzas">
         {pizzaData.map((pizza) => (
           <Pizza
@@ -12,6 +16,7 @@ function Menu() {
             ingredients={pizza.ingredients}
             photoName={pizza.photoName}
             price={pizza.price}
+            soldOut={pizza.soldOut}
           />
         ))}
       </ul>
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,8 +1,8 @@
 // import { pizzaData } from "../data";
 
-function Pizza({ name, ingredients, price, photoName }) {
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
   return (
-    <li className="pizza" key={name}>
+    <li className={soldOut ? "pizza sold-out" : "pizza"} key={name}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
