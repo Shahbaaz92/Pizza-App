6d813d6adc7bf8c0633292027de6c83fd0b339f9
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-05-20 06:11:18 +0530

    Update App

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,44 +4,17 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <section className="pizzas">
-        <Pizza
-          name={pizzaData[0].name}
-          ingredients={pizzaData[0].ingredients}
-          photoName={pizzaData[0].photoName}
-          price={pizzaData[0].price}
-        />
-        <Pizza
-          name={pizzaData[1].name}
-          ingredients={pizzaData[1].ingredients}
-          photoName={pizzaData[1].photoName}
-          price={pizzaData[1].price}
-        />
-        <Pizza
-          name={pizzaData[2].name}
-          ingredients={pizzaData[2].ingredients}
-          photoName={pizzaData[2].photoName}
-          price={pizzaData[2].price}
-        />
-        <Pizza
-          name={pizzaData[3].name}
-          ingredients={pizzaData[3].ingredients}
-          photoName={pizzaData[3].photoName}
-          price={pizzaData[3].price}
-        />
-        <Pizza
-          name={pizzaData[4].name}
-          ingredients={pizzaData[4].ingredients}
-          photoName={pizzaData[4].photoName}
-          price={pizzaData[4].price}
-        />
-        <Pizza
-          name={pizzaData[5].name}
-          ingredients={pizzaData[5].ingredients}
-          photoName={pizzaData[5].photoName}
-          price={pizzaData[5].price}
-        />
-      </section>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza
+            key={pizza.name}
+            name={pizza.name}
+            ingredients={pizza.ingredients}
+            photoName={pizza.photoName}
+            price={pizza.price}
+          />
+        ))}
+      </ul>
     </main>
   );
 }
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,15 +1,15 @@
 // import { pizzaData } from "../data";
 
-function Pizza({ name, ingredients, price, photoName }) {
+function Pizza({ key, name, ingredients, price, photoName }) {
   return (
-    <article className="pizza">
+    <li className="pizza" key={key}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
         <p>{price}</p>
       </div>
-    </article>
+    </li>
   );
 }
 
